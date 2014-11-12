19e9d21092251cb34cbac54b4acb4c20db4da16e
Shaohao Lin <slin232@uwo.ca>
2014-11-12 12:10:01 -0500

    Embedded SoundCloud API

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,13 +1,25 @@
+SC.initialize({
+    client_id: '0e2f4290ff93af6c624a9a7c52526231'
+});
+
 $(document).ready(function() {
 
-	SC.initialize({
-    client_id: "0e2f4290ff93af6c624a9a7c52526231"
-  });
-
-  $("#loadTracks").live("click", function(){
+	//Playing the music live
+  $("#loadTracks").on("click", function(){
     SC.get("/tracks", {limit: 1}, function(tracks){
       var track = tracks[0];
       SC.oEmbed(track.uri, document.getElementById("track"));
     });
   });
+
+  //Get the track of the music with hiphop type	
+  SC.get('/tracks', { genres: 'hiphop' }, function(tracks) {
+    $(tracks).each(function(index, track) {
+      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+    });
+  });
+
+
+  
+
 });
