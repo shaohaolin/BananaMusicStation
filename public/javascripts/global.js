b3cfd565874614c27072f79d5023c606c0238487
Shaohao Lin <slin232@uwo.ca>
2014-11-28 12:19:09 -0500

    Adding Dislike Function

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -21,27 +21,41 @@ $(document).ready(function() {
   		//alert(xmlDoc); 
   		var info = JSON.parse(xmlDoc);
   		tracksListData = info;
+      var tableContent ='';
 
       if (!$('#tracklist').is(":empty")){
         $('#tracklist').empty();
       }
 
-  		$(info).each(function(index, track) {
+  		$(tracksListData).each(function(index, track) {
   			$('#tracklist').append($('<li></li>').html(track.title + ' - ' + track.genre));
         $('#tracklist').append('<div id = tracks'+index+'></div>');
         //oEmbed function() hard code my favourite song to System.
         SC.oEmbed(tracksListData[index].permalink_url, document.getElementById('tracks'+index));
+
+        //table code
+         tableContent += '<tr>';
+         tableContent += '<td>' + track.title + '</td>';
+         tableContent += '<td><a href="#" class="linkdeletesong" rel="' + index + '">Dislike</a></td>';
+         tableContent += '</tr>';
   		});
+
+        // Inject the whole content string into our existing HTML table
+        $('#musicList table tbody').html(tableContent);
+
 
   		//oEmbed function() hard code my favourite song to System.
       //SC.oEmbed(tracksListData[0].permalink_url, document.getElementById("track"));
     });
 });
 
+
+
 $("#hiphop").on("click", getMusic);
 $("#country").on("click", getMusic);
 $("#pop").on("click", getMusic);
 $("#electronic").on("click", getMusic);
+$("#musicList table tbody").on('click','td a.linkdeletesong',deleteThisSong);
 
 function getMusic(event){
 
@@ -58,19 +72,66 @@ function getMusic(event){
       var info = JSON.parse(xmlDoc);
       tracksListData = info;
 
+      var tableContent ='';
+
       if (!$('#tracklist').is(":empty")){
         $('#tracklist').empty();
       }
 
-      $(info).each(function(index, track) {
+      $(tracksListData).each(function(index, track) {
         
         $('#tracklist').append($('<li></li>').html(track.title + ' - ' + track.genre));
         $('#tracklist').append('<div id = tracks'+index+'></div>');
         //oEmbed function() hard code my favourite song to System.
         SC.oEmbed(tracksListData[index].permalink_url, document.getElementById('tracks'+index));
+
+        //table code
+         tableContent += '<tr>';
+         tableContent += '<td>' + track.title + '</td>';
+         tableContent += '<td><a href="#" class="linkdeletesong" rel="' + index + '" >Dislike</a></td>';
+         tableContent += '</tr>';
+
       });
 
+        // Inject the whole content string into our existing HTML table
+        $('#musicList table tbody').html(tableContent);
 };
 
 
+function deleteThisSong(event){
+
+   // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+      
+      var index = $(this).attr('rel') ;
+
+      alert(index);
+      tracksListData.splice(index,1);
+      var tableContent ='';
+      if (!$('#tracklist').is(":empty")){
+        $('#tracklist').empty();
+      }
+
+      $(tracksListData).each(function(index, track) {
+        
+        $('#tracklist').append($('<li></li>').html(track.title + ' - ' + track.genre));
+        $('#tracklist').append('<div id = tracks'+index+'></div>');
+        //oEmbed function() hard code my favourite song to System.
+        SC.oEmbed(tracksListData[index].permalink_url, document.getElementById('tracks'+index));
+
+        //table code
+         tableContent += '<tr>';
+         tableContent += '<td>' + track.title + '</td>';
+         tableContent += '<td><a href="#" class="linkdeletesong" rel="' + index + '" >Dislike</a></td>';
+         tableContent += '</tr>';
+
+      });
+
+        // Inject the whole content string into our existing HTML table
+        $('#musicList table tbody').html(tableContent);
+    }
+};
 
